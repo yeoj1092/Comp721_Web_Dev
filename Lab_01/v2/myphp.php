c02ccc7c707562f7df786b7bf282fcb57c4a90c4
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2023-01-01 13:20:36 +1300

    Create myphp.php

---
+++ Lab_01/v2/myphp.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html xmlns="https://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+   <!-- This line specifies the content type and character encoding for the HTML document -->
+   <meta http-equiv="content-type" content="text/html" ; charset="utf-8" />
+   <!-- This line provides a brief description of the content of the page -->
+   <meta name="description" content="web Development :: Lab 1" />
+   <!-- This line specifies the keywords that are relevant to the content of the page -->
+   <meta name="keywords" content="web,development" />
+   <!-- This line specifies the title of the page, which is displayed in the browser's title bar or tab -->
+   <title>My first web page</title>
+</head>
+
+<body>
+   <!-- This is the main heading of the page -->
+   <h1>Web Development Lab 1</h1>
+   <!-- This is a question that is posed on the page -->
+   <p>Can I create a .php file that do not contain any PHP scripts?</p>
+   <!-- This is the answer to the question -->
+   <p>Yes, but this should be avoided, so that the server does not do any unnecessary parsing and processing</p>
+</body>
+
+</html>
