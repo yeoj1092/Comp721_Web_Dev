8b3d73aaaa601a24d97d86348b9f206ed08e1899
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2023-01-06 18:15:32 +1300

    Create vip_member.php

---
+++ Lab_05/v1/vip_member.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Lab05 Task 2 - VIP member Registration System</title>
+  <h1>Web Development - Week 05</h1>
+  <h2> Task 2: VIP member Registration System</h2>
+</head>
+
+<body>
+  <h1>Home Page</h1>
+  <p><a href="member_add_form.php">Add New Member Form</a></p>
+  <p><a href="member_display.php">Display all Members Page</a></p>
+  <p><a href="member_search.php">Search Member Page</a> </p>
+</body>
+
+</html>
