ef91273e9c35b4d487e0eda61ade20b8e31cebfd
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2023-01-07 16:39:40 +1300

    Create member_add_form.php

---
+++ Lab_05/v2/member_add_form.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title>Lab05 Task 2 - VIP member Registration System</title>
+</head>
+
+<body>
+  <h1>Web Development - Week 05</h1>
+  <h2>Task 2: VIP member Registration System</h2>
+  <h3>Add New Member Form</h3>
+
+  <!-- Form to add a new member -->
+  <form method="post" action="member_add.php">
+    First name: <input type="text" name="fname"><br>
+    Last name: <input type="text" name="lname"><br>
+    Gender: <input type="text" name="gender"><br>
+    Email: <input type="text" name="email"><br>
+    Phone: <input type="text" name="phone"><br>
+    <input type="submit" value="Add Member">
+  </form>
+</body>
+
+</html>
