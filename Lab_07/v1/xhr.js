a42fcc85a7818bdc484f22e6f70a0aa3d3b01b6b
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2023-01-10 19:48:50 +1300

    Create xhr.js

---
+++ Lab_07/v1/xhr.js
@@ -0,0 +1,10 @@
+// file xhr.js
+function createRequest() {
+  var xhr = false;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return xhr;
+} // end function createRequest()
